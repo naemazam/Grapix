d6226e3da07fe84cb53b8bb5bcc9829d4c4cf974
Naem Azam <78720392+naemazam@users.noreply.github.com>
2023-04-04 15:08:13 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,86 @@
+var hero = $(".hero_area");
+var menu = $(".custom_menu-container");
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > hero.height()) {
+        menu.addClass("menu_fixed-position");
+        $(".custom_menu-container + section").addClass("mt-5");
+    } else {
+        menu.removeClass("menu_fixed-position");
+        $(".custom_menu-container + section").removeClass("mt-5");
+    }
+});
+
+
+var myCircle = Circles.create({
+    id: "circles-1",
+    radius: 65,
+    value: 85,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-2",
+    radius: 65,
+    value: 55,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-3",
+    radius: 65,
+    value: 65,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-4",
+    radius: 65,
+    value: 85,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
